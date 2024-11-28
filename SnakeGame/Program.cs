4b67851c2c6012382d0c774ae7d70305f552a0f5
Struan McLean <struanmclean271107@gmail.com>
2024-11-28 16:16:57 +0000

    First commit. Created basic movment logic.

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace SnakeGame 
+{
+    class Start
+    {
+        static void Main()
+        {
+            Console.CursorVisible = false;
+
+            CreateGame game = new CreateGame();
+            game.StartGame();
+        }
+    }
+
+    class CreateGame
+    {
+        private int width = 20;
+        private int height = 20;
+        private int score;
+
+        public void StartGame()
+        {
+            score = 0;
+
+            while (true) 
+            {
+                Input();
+                Thread.Sleep(300);
+            }
+        }
+
+        private void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.W:
+                        Console.WriteLine("UP");
+                        break;
+                    case ConsoleKey.A:
+                        Console.WriteLine("LEFT");
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine("RIGHT");
+                        break;
+                    case ConsoleKey.S:
+                        Console.WriteLine("DOWN");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("LEFT");
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("UP");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("DOWN");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("RIGHT");
+                        break;
+                }
+            }
+        }
+    }
+
+    class Snake
+    {
+        // Snake class implementation
+    }
+}
